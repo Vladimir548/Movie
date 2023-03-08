9449b4f88359e3e544f9dc44e8235153f237550e
vova <vovar>
2023-03-08 21:45:53 +0400

    add await films

--- src/components/movie/movieSearch/MovieSearchItem.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import { IMovie, IMovieResponse } from '../../../models/movie.interface'
-
-export const MovieSearchItem: FC<{ movieSearch: IMovie }> = ({
-	movieSearch
-}) => {
-	return <div>{movieSearch.name}</div>
-}
